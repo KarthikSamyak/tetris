13d4fe7167b3140d26c8c7ae875b5779d193a1c7
Karthik samyak <karthik.samyak@outlook.com>
2018-02-26 01:53:25 +0530

    basic logic implemented with request animation frame

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ var width = canvas.width;
 var height = canvas.height;
 
 var grids = [];
+var tetriminos = [];
+
+var tBlock = new Block(random(40,560),0, random(0,4),"red");
+tetriminos.push(tBlock);
 
 function Block(x, y, id, color) {
     this.x = x;
     this.y = y;
     this.id = id;
     this.color = color;
+    this.fixed = false;
 }
 
 Block.prototype.draw = function() {
@@ -18,21 +23,50 @@ Block.prototype.draw = function() {
 }
 
 Block.prototype.update = function() {
-    if(checkGrid(this.x,this.y + 40)) {
+    if(checkGrid(this.x,this.y + 40) && this.y + 40 < height) {
         this.y = this.y + 40;
     }
-    else {
+    else if(this.fixed == false) {
         this.y = this.y;
         updateGrid(this.x,this.y);
+        console.log(this.x, this.y);
+        createNewTetrimino();
+        this.fixed = true;
+    } else {
+        this.y = this.y;
     }
 
+}
+
+
+/*---------------------------- Initialization function ------------------------*/
+function init() {
+    Grids();
+    loop();
+
+}
+
+/*-------------------- function to create new tetrimino when predecessor occupies a grid-----------*/
+function createNewTetrimino() {
+    tBlock = new Block(random(40,560),0, random(0,4),"red");
+    tetriminos.push(tBlock);
 }
 
 /*------------------------ Function to create random number ----------------------*/
 function random(min, max) {
+    var rem;
     var num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
+    if(num > 40) {
+        rem = num % 40;
+        num = num - rem;
+        return num;
+    } else {
+        rem = 40 % num;
+        num = num + rem;
+        return num;
+    }
 }
+
 
 /*--------------------- Forming and defining grids --------------------------*/
 function Grids() {
@@ -66,12 +100,22 @@ function checkGrid(x,y) {
 function updateGrid(x,y) {
     for(let i=0; i<grids.length; i++) {
         if(grids[i].x == x && grids[i].y == y) {
-            grids[i].free == false;
+            grids[i].free = false;
         }
     }
 }
 
-var testBlock = new Block(40, 0, 4, 'blue');
+function loop() {
+    ctx.clearRect(0,0,600,600);
+    debugger;
+    
+    for(let i=0; i<tetriminos.length; i++) {
+        tetriminos[i].draw();
+        tetriminos[i].update();
+    }
 
-testBlock.draw();
+    requestAnimationFrame(loop); 
+}
+
+init();
 
