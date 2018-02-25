46422442e2e2a68740f4dd668542e50176a6b65c
Karthik samyak <karthik.samyak@outlook.com>
2018-02-25 18:24:57 +0530

    project created

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CanvasComponent } from './components/canvas/canvas.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CanvasComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
